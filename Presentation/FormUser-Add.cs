8963f44a9da5fa4d14bfeef9acf66f4fff75785a
JuanRojas Home <juandarojas01@gmail.com>
2020-11-12 19:26:23 -0500

    Validar campo formato de correo

--- Presentation/FormUser-Add.cs
+++ Presentation/FormUser-Add.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Domain;
@@ -51,19 +52,25 @@ namespace Presentation
         {
             if (value == false)
             {
-                int va = objetuser.AddUser(txtLoginName.Text, txtUserPass.Text, txtFirstName.Text, txtLastName.Text, cbCargo.Text, txtEmail.Text);
-                if(va == 1)
+                if (validMail(txtEmail.Text))
                 {
-                    MessageBox.Show("Se agrego correctamente");
+                    int va = objetuser.AddUser(txtLoginName.Text, txtUserPass.Text, txtFirstName.Text, txtLastName.Text, cbCargo.Text, txtEmail.Text);
+                    if(va == 1)
+                    {
+                        MessageBox.Show("Se agrego correctamente");
+                        FormUser user = new FormUser();
+                        user.Show();
+                        this.Hide();
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se pudo agregar los datos");
+                    }
                 }
                 else
                 {
-                    MessageBox.Show("No se pudo agregar los datos");
+                    MessageBox.Show("Escribe una dirección de correo electrónico válida.");
                 }
-
-                FormUser user = new FormUser();
-                user.Show();
-                this.Hide();
             }
             if (value)
             {
@@ -80,6 +87,27 @@ namespace Presentation
                 FormUser user = new FormUser();
                 user.Show();
                 this.Hide();
+            }
+        }
+
+        private Boolean validMail(String email)
+        {
+            String expresion;
+            expresion = "\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*";
+            if (Regex.IsMatch(email, expresion))
+            {
+                if (Regex.Replace(email, expresion, String.Empty).Length == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
             }
         }
     }
